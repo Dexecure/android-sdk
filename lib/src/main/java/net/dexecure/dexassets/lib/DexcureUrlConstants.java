a1e9303a8c74c8552e922d6c454c864a7abb4585
imsaurabhm <32478827+imsaurabhm@users.noreply.github.com>
2019-06-10 18:12:23 +0530

    2nd update

--- lib/src/main/java/net/dexecure/dexassets/lib/DexcureUrlConstants.java
+++ lib/src/main/java/net/dexecure/dexassets/lib/DexcureUrlConstants.java
@@ -2,17 +2,20 @@ package net.dexecure.dexassets.lib;
 
 public interface DexcureUrlConstants {
 
+    //TODO for keys
     String RESIZE = "resize";
-    String CROP_MODE = "crop";
-    String CROP_CENTER = "center";
-    String RESIZE_NONE = "none";
-    String WIDTH="w";
-    String HEIGHT="h";
+    String CROP_MODE = "crop_mode";
+    String OPTIMIZATION_MODE = "optimization_mode";
 
-    String OPTIMIZATION_MODE = "opt";
-    String OPTIMIZATION_DEFAULT = "default";
-    String OPTIMIZATION_MILD = "mild";
-    String OPTIMIZATION_AGGRESSIVE = "aggressive";
-    String OPTIMIZATION_NONE = "none";
+    //TODO for values
+    String CROP_CENTER = "resize_c=";
+    String RESIZE_NONE = "resize=none";
+    String OPTIMIZATION_DEFAULT = "opt=default";
+    String OPTIMIZATION_MILD = "opt=mild";
+    String OPTIMIZATION_AGGRESSIVE = "opt=aggressive";
+    String OPTIMIZATION_NONE = "opt=none";
+
+    String WIDTH = "w";
+    String HEIGHT = "h";
 
 }
