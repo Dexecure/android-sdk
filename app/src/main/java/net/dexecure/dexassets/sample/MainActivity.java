70de911e8d4d90e14ef2a6f8d41490770c102cdc
imsaurabhm <32478827+imsaurabhm@users.noreply.github.com>
2019-06-10 18:23:11 +0530

    2nd update

--- app/src/main/java/net/dexecure/dexassets/sample/MainActivity.java
+++ app/src/main/java/net/dexecure/dexassets/sample/MainActivity.java
@@ -9,37 +9,50 @@ import net.dexecure.dexassets.lib.DexecureURLBuilder;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.CROP_CENTER;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.CROP_MODE;
 import static net.dexecure.dexassets.lib.DexcureUrlConstants.HEIGHT;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.OPTIMIZATION_AGGRESSIVE;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.OPTIMIZATION_DEFAULT;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.OPTIMIZATION_MILD;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.OPTIMIZATION_MODE;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.OPTIMIZATION_NONE;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.RESIZE;
+import static net.dexecure.dexassets.lib.DexcureUrlConstants.RESIZE_NONE;
 import static net.dexecure.dexassets.lib.DexcureUrlConstants.WIDTH;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Map<String, String> params = new LinkedHashMap<>();
-        //params.put(RESIZE, "");
-        //params.put(RESIZE_WITH_CENTER_CROP, "");
-        //params.put(DISABLE_RESIZE,"");
-        //params.put(OPTIMIZATION_DEFAULT, "");
-        //params.put(OPTIMIZATION_MILD, "");
-        //params.put(OPTIMIZATION_AGGRESSIVE, "");
-        //params.put(OPTIMIZATION_NONE,"");
+        DexecureURLBuilder urlBuilder = new DexecureURLBuilder("beek.dexecure.net");
+
+        //urlBuilder.setParameter(WIDTH,"200");
+        //urlBuilder.setParameter(HEIGHT,"300");
+
+        //urlBuilder.setParameter("custom","hello world");
+        //urlBuilder.setParameter("hello world","custom");
+        //urlBuilder.setParameter("custom","value");
+        //urlBuilder.setHttps(false);
+        //urlBuilder.setParameter("custom2","value2");
+        //urlBuilder.setParameter(RESIZE, RESIZE_NONE);
+        //urlBuilder.setParameter(CROP_MODE, CROP_CENTER);
+        //urlBuilder.setParameter(OPTIMIZATION_MODE, OPTIMIZATION_DEFAULT);
+        //urlBuilder.setParameter(OPTIMIZATION_MODE, OPTIMIZATION_MILD);
+        //urlBuilder.setParameter(OPTIMIZATION_MODE, OPTIMIZATION_AGGRESSIVE);
+        //urlBuilder.setParameter(OPTIMIZATION_MODE, OPTIMIZATION_NONE);
 
 
-        //params.put(HEIGHT, "200");
-        //params.put(WIDTH, "300");
 
-        //params.put("sdfsdfcd", "plkgdj");
+        //urlBuilder.setParameter(WIDTH,"200");
+        //urlBuilder.setParameter(HEIGHT,"300");
 
-        DexecureURLBuilder urlMaker = new DexecureURLBuilder("beek.dexecure.net", "/proxy/https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg",true);
-        urlMaker.setParameter(HEIGHT,200);
-        urlMaker.setParameter(WIDTH,300);
-        System.out.println(urlMaker.getURL());
 
+
+        System.out.println(urlBuilder.createURL("/proxy/https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg"));
 
     }
 }
